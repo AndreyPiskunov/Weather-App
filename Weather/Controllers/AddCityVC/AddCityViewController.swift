5a5837ff9a15d9e6f896010beabbb270eb78a6f2
Andrey91 <mistaandrey@icloud.com>
2022-10-22 17:45:57 +0500

    Add city view controller

---
+++ Weather/Controllers/AddCityVC/AddCityViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AddCityViewController.swift
+//  Weather
+//
+//  Created by Andrey Piskunov on 22.10.2022.
+//
+
+import UIKit
+
+class AddCityViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
