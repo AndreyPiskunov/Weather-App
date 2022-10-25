6422b87c8ba304cbfc091e86aeb9c00df2f6e586
Andrey91 <mistaandrey@icloud.com>
2022-10-25 18:09:10 +0500

    Add CityListDataBaseCreator.xcdatamodel

--- Weather/Controllers/AddCityVC/AddCityViewController.swift
+++ Weather/Controllers/AddCityVC/AddCityViewController.swift
@@ -6,16 +6,36 @@
 //
 
 import UIKit
+import CoreData
 
 class AddCityViewController: UIViewController {
+    
+    // holding string values displayed by the table view
+    var cities: [NSManagedObject] = []
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var resultsSearchTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         searchBar.searchTextField.backgroundColor = .white
+        
+//        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+//                     return
+//                 }
+//        // Get Managed Context Object with help of AppDelegate.
+//                 let managedContext = appDelegate.persistentContainer.viewContext
+//
+//                 let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "City")
+//
+//                 do {
+//                     cities = try managedContext.fetch(fetchRequest)
+//                 } catch {
+//                     print(error)
+//                 }
+//
+//                 print(cities)
     }
     
     @IBAction func cancelButtonPressed(_ sender: Any) {
