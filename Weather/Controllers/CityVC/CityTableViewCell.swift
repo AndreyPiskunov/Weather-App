c6b0f92c6d08e35ba90f534e2ec94a46ab51f0f5
Andrey91 <mistaandrey@icloud.com>
2022-10-29 18:10:24 +0500

    Add fix cell shadows

--- Weather/Controllers/CityVC/CityTableViewCell.swift
+++ Weather/Controllers/CityVC/CityTableViewCell.swift
@@ -18,9 +18,8 @@ class CityTableViewCell: UITableViewCell {
     @IBOutlet weak var degreeLabel: UILabel!
     @IBOutlet weak var weatherBackgroundView: UIView!
     
-    
-    
     private let gradient = CAGradientLayer()
+    private let cellShapeMask = UIView()
     
     override var isHighlighted: Bool {
              didSet{
@@ -31,19 +30,20 @@ class CityTableViewCell: UITableViewCell {
     
     // MARK: - Lifecycle
          
-         override func layoutIfNeeded() {
-             super.layoutIfNeeded()
+    override func layoutIfNeeded() {
+        super.layoutIfNeeded()
+        
+        //Configuring gradient frame when views calculating
+        gradient.frame = weatherBackgroundView.bounds
+    }
 
-             //Configuring gradient frame when views calculating
-             gradient.frame = weatherBackgroundView.bounds
-         }
-
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         
         self.selectionStyle = .none
         
+        //Setting up cell shape
+        setCellComponentShape(view: weatherBackgroundView.layer)
         
         //Making shadow
         weatherBackgroundView.layer.shadowColor = UIColor.black.cgColor
@@ -63,21 +63,26 @@ class CityTableViewCell: UITableViewCell {
         setGradient(withStyle: .blank)
 
         weatherBackgroundView.layer.insertSublayer(gradient, at: 0) //Adding gradient
-                 
-        
-        
         
     }
     
-    func setGradient(withStyle style: GradientColors){
-            switch style {
-                     case .day: gradient.colors = [UIColor(red: 68/255, green: 166/255, blue: 252/255, alpha: 1).cgColor,
-                                                   UIColor(red: 114/255, green: 225/255, blue: 253/255, alpha: 1).cgColor]
-                     case .fog: break
-                     case .night: gradient.colors = [UIColor(red: 9/255, green: 7/255, blue: 40/255, alpha: 1).cgColor,
-                                                     UIColor(red: 30/255, green: 94/255, blue: 156/255, alpha: 1).cgColor]
-                     case .blank: gradient.colors = nil
-                     }
+    func setGradient(withStyle style: GradientColors) {
+        
+        switch style {
+        case .day: gradient.colors = [UIColor(red: 68/255, green: 166/255, blue: 252/255, alpha: 1).cgColor,
+                                      UIColor(red: 114/255, green: 225/255, blue: 253/255, alpha: 1).cgColor]
+        case .fog: break
+        case .night: gradient.colors = [UIColor(red: 9/255, green: 7/255, blue: 40/255, alpha: 1).cgColor,
+                                        UIColor(red: 30/255, green: 94/255, blue: 156/255, alpha: 1).cgColor]
+        case .blank: gradient.colors = nil
+        }
+    }
+    
+    private func setCellComponentShape(view: CALayer) {
+        
+        //Making proper round corners
+        view.cornerCurve = CALayerCornerCurve.continuous
+        view.cornerRadius = 15
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
